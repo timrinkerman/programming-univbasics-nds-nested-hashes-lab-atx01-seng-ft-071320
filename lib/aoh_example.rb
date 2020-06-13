6cb440db82b06728609d13be9485a3d91a539b5b
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 17:19:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,7 @@ def assembled_aoh
   KATE_G,
   BRUCE_G
 ]
- 
+film_fest 
   
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
