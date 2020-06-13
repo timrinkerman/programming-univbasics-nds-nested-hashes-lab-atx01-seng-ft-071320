97f6a1bbd0eb99e54f43589f04e039888da4e13a
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 17:24:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,8 +25,8 @@ def literal_aoh
    {:name => "Don Gately", 
    :occupation => "Live-in Staff" },
    {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
-   {:name => "Pat Monteseian", :occupation => "Staff" }
-   {:name => "Kate Gompert", :occupation => "None"}
+   {:name => "Pat Monteseian", :occupation => "Staff" },
+   {:name => "Kate Gompert", :occupation => "None"},
     {:name => "Bruce Green", :occupation => "Fan of Mildred" }
 
    ]
