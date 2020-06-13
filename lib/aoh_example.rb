672565bfafe3bbaa3c795ade5adb8d5d7153809f
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 17:24:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,7 +24,7 @@ def literal_aoh
  film_fest = [
    {:name => "Don Gately", 
    :occupation => "Live-in Staff" },
-   {:name => "Joelle van Dyne", :occupation => "Radio Personality" }
+   {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
    {:name => "Pat Monteseian", :occupation => "Staff" }
    {:name => "Kate Gompert", :occupation => "None"}
     {:name => "Bruce Green", :occupation => "Fan of Mildred" }
